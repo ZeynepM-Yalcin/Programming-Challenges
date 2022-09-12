08b96a34573e3827a843a1ec7e97a8b725b04cb9
ZeynepM-Yalcin <113379342+ZeynepM-Yalcin@users.noreply.github.com>
2022-09-12 19:08:45 +0100

    Add files via upload

---
+++ Challenge 1/Rod_conversions.py
@@ -0,0 +1,39 @@
+def rod_to_meters(rods):
+    meters = rods * 5.0292
+    return meters
+
+def meters_to_feet(meters):
+    feet = meters / 0.3048
+    return feet
+    
+def meters_to_miles(meters):
+    miles = meters / 1609.34
+    return miles
+
+def rods_to_furlongs(rods):
+    furlongs = rods / 40
+    return furlongs
+
+def miles_in_minutes(miles):
+    minutes = miles / 60
+    return minutes
+    
+def calculate():
+    rods = float(input("Please enter value: "))
+    meters = rod_to_meters(rods)
+    feet = meters_to_feet(meters)
+    miles = meters_to_miles(meters)
+    furlongs = rods_to_furlongs(rods)
+    minutes = miles_in_minutes(miles)
+
+    return rods,meters, feet, miles, furlongs, minutes
+
+
+rods,meters, feet, miles, furlongs, minutes = calculate()
+print(f" meters: {meters}")
+print(f" feet: {feet}")
+print(f" miles: {miles}")
+print(f" furlongs: {furlongs}")
+print(f" Minutes to walk {rods}: {minutes}")
+
+
