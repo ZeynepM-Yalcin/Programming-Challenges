1b79284bd9945e9b87017e2348ad63647956d8af
ZeynepM-Yalcin <113379342+ZeynepM-Yalcin@users.noreply.github.com>
2022-09-14 13:51:48 +0100

    Update test 2.py

--- Challenges 2/test 2.py
+++ Challenges 2/test 2.py
@@ -9,9 +9,9 @@ class MyFirstTests(unittest.TestCase):
         joules = in_joules(2)
         self.assertEqual(joules, 63095734.448019296)
 
-    def test_in_TNT(self):
-        TNT = in_TNT(2)
-        self.assertEqual(TNT, 4.780114722753346e-10)
+    def test_in_tnt(self):
+        tnt = in_tnt(2)
+        self.assertEqual(tnt, 4.780114722753346e-10)
 
 if __name__ == '__main__':
     unittest.main()
